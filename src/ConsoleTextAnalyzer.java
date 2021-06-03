1b8e15c65c34e94442a5a65f67b57664a50e0b1b
JelenaFr <jelena.freiberg@gmail.com>
2021-06-03 16:29:00 +0300

    ok

--- src/ConsoleTextAnalyzer.java
+++ src/ConsoleTextAnalyzer.java
@@ -2,43 +2,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class ConsoleTextAnalyzer {public static void main(String[] args) throws IOException {
-    ConsoleTextAnalyzer consoleTextAnalyzer = new ConsoleTextAnalyzer();
-    HashMap<String, String> params = consoleTextAnalyzer.convertToKeyValuePair(args);
+public class ConsoleTextAnalyzer {
+    public static void main(String[] args) throws IOException {
+        ConsoleTextAnalyzer consoleTextAnalyzer = new ConsoleTextAnalyzer();
+        Map<String, String> params = consoleTextAnalyzer.convertToKeyValuePair(args);
+        if (params.containsKey("-F")) {
+            String[] allArgsFiles = params.get("-F").split(",");
 
+            for (int i = 0; i < allArgsFiles.length; i++) {
+                BufferedReader br = new BufferedReader(new FileReader(allArgsFiles[i]));
+                String readFileContext;
+                String line;
+                while ((readFileContext = br.readLine()) != null) {
+                    line = readFileContext.replaceAll("\\p{Punct}", " ").trim();
+                    System.out.println(line);
+                    List<String> arrFromTextFile = Arrays.asList(line.split(" "));
 
-
-    if (params.containsKey("-F")) {
-        String[] allArgsFiles  = params.get("-F").split(",");
-        String[] allStopWords  = params.get("-S").split(",");
-
-
-        for (int i = 0; i < allArgsFiles.length; i++) {
-            BufferedReader br = new BufferedReader(new FileReader(allArgsFiles[i]));
-            String readFileContext;
-            String context;
-            while ((readFileContext = br.readLine()) != null) {
-                context = readFileContext.replaceAll("\\p{Punct}", " ").trim();
-                System.out.println(context);
-                List<String> arrFromTextFile = Arrays.asList(context.split(" "));
-
-                consoleTextAnalyzer.getAnalysisResult(params, arrFromTextFile, allStopWords, consoleTextAnalyzer.capitalizeWordFilter(params, context));
+                    consoleTextAnalyzer.getAnalysisResult(params, arrFromTextFile, consoleTextAnalyzer.getStopwords(params, line));
+                }
+                br.close();
             }
-            br.close();
+
+        } else {
+            System.out.println("Not correct file arguments");
         }
-
     }
-    else {System.out.println("Not correct file arguments");}
-
-
-}
-
-    private HashMap<String, String> convertToKeyValuePair(String[] args) {
-        LinkedHashMap<String, String> params = new LinkedHashMap<>();
+    private Map<String, String> convertToKeyValuePair(String[] args) {
+        Map<String, String> params = new HashMap<>();
         for (String arg : args) {
             if (arg.contains("=")) {
                 String[] splitFromEqual = arg.split("=");
@@ -50,63 +42,40 @@ public class ConsoleTextAnalyzer {public static void main(String[] args) throws 
         }
         return params;
     }
-
-    private List<String> capitalizeWordFilter(HashMap<String, String> params, String str) {
-        List<String> capitalizeWords = new ArrayList<>();
-
-        if (params.containsKey("-L")) {
-            Pattern pattern = Pattern.compile("[A-Z][a-z]+");
-            Matcher matcher = pattern.matcher(str);
-            while (matcher.find()) {
-                capitalizeWords.add(matcher.group());
-            }
-        }
-
-        return capitalizeWords;
-    }
-
-
-
-
-    public void getAnalysisResult(HashMap<String, String> params, List<String> arr,String[] allStopWords, List<String> capitalizeWords) {
-
-
+    private void getAnalysisResult(Map<String, String> params, List<String> arr, List<String> allStopWords) {
+        List<String> filteredWords = new ArrayList<>();
         if (params.size() == 1) {
-            System.out.println("No Flags => " + arr.size() + " Words");
+            System.out.print("No Flags => ");
         }
-        if  (params.size() >= 2) {
-            ConsoleTextAnalyzer consoleTextAnalyzer = new ConsoleTextAnalyzer();
 
-            List<String> stopWords = Arrays.asList(allStopWords);
-            List<String> filteredWords = arr.stream()
-
-                    .filter(k -> !stopWords.contains(k.toLowerCase(Locale.ROOT)))
-                    .filter(k -> capitalizeWords.contains(k))
-                    .collect(Collectors.toList());
-
-            filteredWords.forEach(n-> System.out.print(n+" "));
-            System.out.println();
-            System.out.println(filteredWords.size() + " words");
-
-            if (checkFlagContain(params, "-C")) {
-                System.out.println( consoleTextAnalyzer.countCharacters(filteredWords)+" symbols");
-
+        if (params.size() >= 2) {
+            if (params.containsKey("-L")) {
+                for (int i = 0; i < arr.size(); i++) {
+                    if (Character.isUpperCase(arr.get(i).charAt(0))) {
+                        filteredWords.add(arr.get(i));
+                    }
+                }
+                arr = filteredWords;
             }
+
+            if (params.containsKey("-S")) {
+                filteredWords = arr.stream()
+                        .map(String::toLowerCase)
+                        .filter(k -> !allStopWords.contains(k))
+                        .collect(Collectors.toList());
+                arr = filteredWords;
+            }
+
         }
 
 
-
-
-
-
+        System.out.println(arr.size() + " words");
+        //System.out.println(arr);
+        if (params.containsKey("-C")) {
+            System.out.println(countCharacters(arr) + " symbols");
+        }
 
     }
-
-    private boolean checkFlagContain(HashMap<String, String> params, String flag) {
-
-        return params.containsKey(flag);
-    }
-
 
     private int countCharacters(List<String> arr) {
         String result = arr.stream()
@@ -115,4 +84,11 @@ public class ConsoleTextAnalyzer {public static void main(String[] args) throws 
         return result.length();
     }
 
+    private List<String> getStopwords(Map<String, String> params, String line) {
+        List<String> allStopWords = new ArrayList<>();
+        if (params.containsKey("-S")) {
+            allStopWords = Arrays.asList(params.get("-S").split(","));
+        }
+        return allStopWords;
+    }
 }
