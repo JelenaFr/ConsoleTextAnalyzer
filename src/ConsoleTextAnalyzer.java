1fc4f5e3560b3b8e36e0faeaa72954b55c35f630
JelenaFr <jelena.freiberg@gmail.com>
2021-06-04 22:51:34 +0300

    clean code

--- src/ConsoleTextAnalyzer.java
+++ src/ConsoleTextAnalyzer.java
@@ -3,26 +3,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ConsoleTextAnalyzer {
+    String argF= "-F";
+    String argC= "-C";
+    String argL= "-L";
+    String argS= "-S";
     public static void main(String[] args) {
+
         ConsoleTextAnalyzer consoleTextAnalyzer = new ConsoleTextAnalyzer();
         Map<String, String> params = consoleTextAnalyzer.convertToKeyValuePair(args);
         List<String> stopWords = consoleTextAnalyzer.findStopwords(params);
-        String[] argsF = params.get("-F").split(",");
-        if (params.containsKey("-F")) {
-            for (int i = 0; i < argsF.length; i++) {
-                String fileText = consoleTextAnalyzer.readfile(argsF[i]);
-                List<String> text = Arrays.asList(fileText.split(" "));
-                try {
-                    consoleTextAnalyzer.getAnalysisResult(params, text, stopWords);
-                } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Empty file");
-                }
+        consoleTextAnalyzer.startAnalyze(params, stopWords);
 
-            }
-
-        } else {
-            System.out.println("Correct file arguments not found");
-        }
     }
 
     private Map<String, String> convertToKeyValuePair(String[] args) {
@@ -30,7 +21,7 @@ public class ConsoleTextAnalyzer {
         for (String arg : args) {
             if (arg.contains("=")) {
                 String[] splitFromEqual = arg.split("=");
-                String key = splitFromEqual[0].substring(0);
+                String key = splitFromEqual[0];
                 String value = splitFromEqual[1];
                 params.put(key, value);
             } else {
@@ -40,35 +31,34 @@ public class ConsoleTextAnalyzer {
         return params;
     }
 
-    private void getAnalysisResult(Map<String, String> params, List<String> arr, List<String> allStopWords) {
+    private void getAnalysisResult(Map<String, String> params, List<String> text, List<String> stopWords) {
         List<String> filteredWords = new ArrayList<>();
         if (params.size() == 1) {
             System.out.print("No Flags => ");
         }
 
         if (params.size() >= 2) {
-            if (params.containsKey("-L")) {
-                for (int i = 0; i < arr.size(); i++) {
-                    if (Character.isUpperCase(arr.get(i).charAt(0))) {
-                        filteredWords.add(arr.get(i));
+            if (params.containsKey(argL)) {
+                for (String s : text) {
+                    if (Character.isUpperCase(s.charAt(0))) {
+                        filteredWords.add(s);
                     }
                 }
-                arr = filteredWords;
+                text = filteredWords;
             }
-            if (params.containsKey("-S")) {
-                filteredWords = arr.stream()
+            if (params.containsKey(argS)) {
+                filteredWords = text.stream()
                         .map(String::toLowerCase)
-                        .filter(k -> !allStopWords.contains(k))
+                        .filter(k -> !stopWords.contains(k))
                         .collect(Collectors.toList());
-                arr = filteredWords;
+                text = filteredWords;
             }
-
         }
-        System.out.println(arr);
-        System.out.println(arr.size() + " words");
+        //System.out.println(arr);
+        System.out.println(text.size() + " words");
 
-        if (params.containsKey("-C")) {
-            System.out.println(countCharacters(arr) + " symbols");
+        if (params.containsKey(argC)) {
+            System.out.println(countCharacters(text) + " symbols");
         }
     }
 
@@ -79,36 +69,45 @@ public class ConsoleTextAnalyzer {
 
     private List<String> findStopwords(Map<String, String> params) {
         List<String> allStopWords = new ArrayList<>();
-        if (params.containsKey("-S")) {
+        if (params.containsKey(argS)) {
             allStopWords = Arrays.asList(params.get("-S").split(","));
         }
         return allStopWords;
     }
 
-
-    private String readfile(String fileName) {
+    private String readFile(String file) {
         String line = null;
-        try (Reader reader = new InputStreamReader(new FileInputStream(fileName))) {
+        try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
             int a = reader.read();
             StringBuilder result = new StringBuilder();
             while (a > 0) {
                 result.append((char) a);
                 a = reader.read();
-
-
             }
             String str = result.toString();
             line = str.replaceAll("[^\\p{L}]+", " ");
-            System.out.println(line);
+            //System.out.println(line);
         } catch (FileNotFoundException e) {
-
             System.out.println("File Not Found");
         } catch (IOException e) {
             System.out.println("Wrong arguments");
         }
-
         return line;
     }
-
-
+    public void startAnalyze (Map<String, String> params, List<String> stopWords ) {
+        if (params.containsKey(argF)) {
+            String[] argsF = params.get(argF).split(",");
+            for (String file : argsF) {
+                String fileText = readFile(file);
+                List<String> text = Arrays.asList(fileText.split(" "));
+                try {
+                    getAnalysisResult(params, text, stopWords);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("Empty file");
+                }
+            }
+        } else {
+            System.out.println("Correct file arguments not found");
+        }
+    }
 }
